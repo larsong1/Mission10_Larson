b3fff2245e5e03ad8305c001618809cd05d19cfe
Gabriel Larson <72903557+larsong1@users.noreply.github.com>
2024-03-16 10:20:20 -0600

    Add files via upload

---
+++ backend/BowlingAPI/Controllers/BowlerInfoController.cs
@@ -0,0 +1,25 @@
+﻿using BowlingAPI.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BowlingAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BowlerInfoController : ControllerBase
+    {
+        private IBowlerRepository _bowlerRepository;
+        public BowlerInfoController(IBowlerRepository bowlerRepository)
+        {
+            _bowlerRepository = bowlerRepository;
+        }
+
+        [HttpGet]
+        public IEnumerable<Bowler> Get()
+        {
+            var bowlerData = _bowlerRepository.Bowlers.ToArray();
+
+            return bowlerData;
+        }
+    }
+}
